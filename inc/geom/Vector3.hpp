690eb27d3a38a496450e62aae57ff83a505f3b05
tvarney <troy.a.varney@gmail.com>
2015-01-21 15:24:50 -0600

    Added addition, subtraction and multiplication to Vector3.

--- inc/geom/Vector3.hpp
+++ inc/geom/Vector3.hpp
@@ -97,15 +97,73 @@ namespace geom {
       return *this;
     }
     
+		/**
+		 * \brief Vector negation
+		 * \return A new \c Vector3 object which is the negation of this \c Vector3
+		 */
+		Vector3<Scalar> operator-() {
+			return Vector3<Scalar>(-x,-y,-z);
+		}
+		
     /**
      * \brief Assign the result of adding two vectors.
      * \arg \c rhs The vector to add to this \c Vector3 object
      * \return A reference to the \c Vector3 object being assigned to
      */
-    Vector3<Scalar> & operator+=(const Vector3<Scalar> &rhs) {
+		template <typename RhsType>
+    Vector3<Scalar> & operator+=(const Vector3<RhsType> &rhs) {
       return *this = *this + rhs;
     }
-
+		
+    /**
+     * \brief Assign the result of subtracting two vectors.
+     * \arg \c rhs The vector to subtract from this \c Vector3 object
+     * \return A reference to the \c Vector3 object being assigned to
+     */
+		template <typename RhsType>
+		Vector3<Scalar> & operator-=(const Vector3<RhsType> &rhs) {
+			return *this = *this - rhs;
+		}
+		
+    /**
+     * \brief Assign the result of multiplying two vectors.
+     * \arg \c rhs The vector to multiply this \c Vector3 object by
+     * \return A reference to the \c Vector3 object being assigned to
+     */
+		template <typename RhsType>
+		Vector3<Scalar> & operator*=(const Vector3<RhsType> &rhs) {
+			return *this = *this * rhs;
+		}
+		
+    /**
+     * \brief Assign the result of dividing two vectors.
+     * \arg \c rhs The vector to divide this \c Vector3 object by
+     * \return A reference to the \c Vector3 object being assigned to
+     */
+		template <typename RhsType>
+		Vector3<Scalar> & operator/=(const Vector3<RhsType> &rhs) {
+			return *this = *this / rhs;
+		}
+		
+    /**
+     * \brief Assign the result of scalar multiplication
+     * \arg \c rhs The scalar to multiply this vector by.
+     * \return A reference to the \c Vector3 object being assigned to
+     */
+		template <typename RhsType>
+		Vector3<Scalar> & operator*=(const RhsType &rhs) {
+			return *this = *this * rhs;
+		}
+		
+    /**
+     * \brief Assign the result of scalar division
+     * \arg \c rhs The scalar to divide this vector by
+     * \return A reference to the \c Vector3 object being assigned to
+     */
+		template <typename RhsType>
+		Vector3<Scalar> & operator/=(const RhsType &rhs) {
+			return *this = *this / rhs;
+		}
     
     Scalar x; /**< The x component of the \c Vector3 object */
     Scalar y; /**< The y component of the \c Vector3 object */
@@ -119,17 +177,104 @@ namespace geom {
   
   /**
    * \brief Add two \c Vector3 objects together and return the result.
-   * \arg \c lhs The operand on the left hand side of the addition symbol
-   * \arg \c rhs The operand on the right hand side of the addition symbol
+   * \arg \c lhs The operand on the left hand side of the expression
+   * \arg \c rhs The operand on the right hand side of the expression
    * \return A new \c Vector3 object that contains the result of adding lhs and
    * rhs
    */
   template <typename RhsType, typename LhsType,
-	    typename Result = typename std::common_type<RhsType,LhsType>::type>
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
   Vector3<Result> operator+(const Vector3<LhsType> &lhs,
-			    const Vector3<RhsType> &rhs)
+														const Vector3<RhsType> &rhs)
   {
     return Vector3<Result>(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+  }
+	
+	  /**
+   * \brief Subtract two \c Vector3 objects and return the result.
+   * \arg \c lhs The operand on the left hand side of the expression
+   * \arg \c rhs The operand on the right hand side of the expression
+   * \return A new \c Vector3 object that contains the result of subtracting
+   * rhs from lhs
+   */
+  template <typename RhsType, typename LhsType,
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
+  Vector3<Result> operator-(const Vector3<LhsType> &lhs,
+														const Vector3<RhsType> &rhs)
+  {
+    return Vector3<Result>(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+  }
+	
+  /**
+   * \brief Multiply two \c Vector3 objects together and return the result.
+   * \arg \c lhs The operand on the left hand side of the expression
+   * \arg \c rhs The operand on the right hand side of the expression
+   * \return A new \c Vector3 object that contains the result of multiplying
+	 * lhs and rhs
+   */
+  template <typename RhsType, typename LhsType,
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
+  Vector3<Result> operator*(const Vector3<LhsType> &lhs,
+														const Vector3<RhsType> &rhs)
+  {
+    return Vector3<Result>(lhs.x * rhs.x, lhs.y * rhs.y, lhs.z * rhs.z);
+  }
+	
+  /**
+   * \brief Divide two \c Vector3 objects together and return the result.
+   * \arg \c lhs The operand on the left hand side of the expression
+   * \arg \c rhs The operand on the right hand side of the expression
+   * \return A new \c Vector3 object that contains the result of dividing lhs
+	 * by rhs
+   */
+  template <typename RhsType, typename LhsType,
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
+  Vector3<Result> operator/(const Vector3<LhsType> &lhs,
+														const Vector3<RhsType> &rhs)
+  {
+    return Vector3<Result>(lhs.x / rhs.x, lhs.y / rhs.y, lhs.z / rhs.z);
+  }
+	
+  /**
+   * \brief Multiply a \c Vector3 object by a scalar.
+   * \arg \c lhs The \c Vector3 object to multiply
+   * \arg \c rhs The scalar to multiply the \c Vector3 object by
+   * \return A new \c Vector3 object that contains the result of multiplying
+	 * lhs by rhs
+   */
+  template <typename RhsType, typename LhsType,
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
+  Vector3<Result> operator*(const Vector3<LhsType> &lhs, const RhsType &rhs)
+  {
+    return Vector3<Result>(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs);
+  }
+	
+  /**
+   * \brief Multiply a \c Vector3 object by a scalar.
+   * \arg \c lhs The scalar to multiply the \c Vector3 object by
+   * \arg \c rhs The \c Vector3 object to multiply
+   * \return A new \c Vector3 object that contains the result of multiplying
+	 * lhs by rhs
+   */
+  template <typename RhsType, typename LhsType,
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
+  Vector3<Result> operator*(const LhsType &lhs, const Vector3<RhsType> &rhs)
+  {
+    return Vector3<Result>(rhs.x * lhs, rhs.y * lhs, rhs.z * rhs);
+  }
+	
+	/**
+   * \brief Divide a \c Vector3 object by a scalar.
+   * \arg \c lhs The \c Vector3 object to divide
+   * \arg \c rhs The scalar to divide the \c Vector3 object by
+   * \return A new \c Vector3 object that contains the result of dividing lhs
+	 * by rhs
+   */
+  template <typename RhsType, typename LhsType,
+						typename Result = typename std::common_type<RhsType,LhsType>::type>
+  Vector3<Result> operator/(const Vector3<LhsType> &lhs, const RhsType &rhs)
+  {
+    return Vector3<Result>(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
   }
 }
 
